db8701651a73dd496f04dbbefa060bfc750a11b4
jigar-greycube <jigar@greycube.in>
2026-02-04 14:48:09 +0530

    feat:update auth code

--- library_management/api.py
+++ library_management/api.py
@@ -1,26 +1,45 @@
 import frappe
-from frappe.auth import LoginManager
+from frappe import auth
 
-@frappe.whitelist(allow_guest=True)
-def mobile_login(usr=None, pwd=None):
-    if not usr or not pwd:
-        frappe.throw("Missing username or password")
-        
-    login_manager = LoginManager()
-    login_manager.authenticate(user=usr, pwd=pwd)
-    login_manager.post_login()
-    
-    user = frappe.get_doc("User", frappe.session.user)
-    
-    # generate API keys if not present
-    if not user.api_key:
-        user.api_key = frappe.generate_hash(length=15)
-        user.api_secret = frappe.generate_hash(length=15)
-        user.save(ignore_permissions=True)
-        
-    return {
-        "api_key": user.api_key,
-        "api_secret": user.api_secret,
-        "user": user.name,
-        "full_name": user.full_name,
+@frappe.whitelist( allow_guest=True )
+def mobile_login(usr, pwd):
+    try:
+        login_manager = frappe.auth.LoginManager()
+        login_manager.authenticate(user=usr, pwd=pwd)
+        login_manager.post_login()
+    except frappe.exceptions.AuthenticationError:
+        frappe.clear_messages()
+        frappe.local.response["message"] = {
+            "success_key":0,
+            "message":"Authentication Error!"
+        }
+
+        return
+
+    api_generate = generate_keys(frappe.session.user)
+    user = frappe.get_doc('User', frappe.session.user)
+
+    frappe.response["message"] = {
+        "success_key":1,
+        "message":"Authentication success",
+        "sid":frappe.session.sid,
+        "api_key":user.api_key,
+        "api_secret":api_generate,
+        "username":user.username,
+        "email":user.email
     }
+
+
+
+def generate_keys(user):
+    user_details = frappe.get_doc('User', user)
+    api_secret = frappe.generate_hash(length=15)
+
+    if not user_details.api_key:
+        api_key = frappe.generate_hash(length=15)
+        user_details.api_key = api_key
+
+    user_details.api_secret = api_secret
+    user_details.save()
+
+    return api_secret
